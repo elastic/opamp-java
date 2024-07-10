b0cb53143a30389b9d4ac369d913c345415f154a
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-07-10 14:23:06 +0200

    Implementing SequenceNumberVisitor

--- src/main/java/io/opentelemetry/opamp/client/visitors/SequenceNumberVisitor.java
+++ src/main/java/io/opentelemetry/opamp/client/visitors/SequenceNumberVisitor.java
@@ -3,6 +3,10 @@ package io.opentelemetry.opamp.client.visitors;
 import opamp.proto.Opamp;
 
 public class SequenceNumberVisitor implements AgentToServerVisitor {
+  private int sequenceNumber = 0;
+
   @Override
-  public void visit(Opamp.AgentToServer.Builder builder) {}
+  public void visit(Opamp.AgentToServer.Builder builder) {
+    builder.setSequenceNum(++sequenceNumber);
+  }
 }
