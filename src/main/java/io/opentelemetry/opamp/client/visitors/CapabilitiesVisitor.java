f61c5c8c7f32a0e40e672ccc70875f4240ab5a18
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-07-10 14:47:18 +0200

    Implementing CapabilitiesVisitor

--- src/main/java/io/opentelemetry/opamp/client/visitors/CapabilitiesVisitor.java
+++ src/main/java/io/opentelemetry/opamp/client/visitors/CapabilitiesVisitor.java
@@ -3,6 +3,13 @@ package io.opentelemetry.opamp.client.visitors;
 import opamp.proto.Opamp;
 
 public class CapabilitiesVisitor implements AgentToServerVisitor {
+
   @Override
-  public void visit(Opamp.AgentToServer.Builder builder) {}
+  public void visit(Opamp.AgentToServer.Builder builder) {
+    builder.setCapabilities(
+        Opamp.AgentCapabilities.AgentCapabilities_ReportsStatus_VALUE
+            | Opamp.AgentCapabilities.AgentCapabilities_AcceptsRemoteConfig_VALUE
+            | Opamp.AgentCapabilities.AgentCapabilities_ReportsEffectiveConfig_VALUE
+            | Opamp.AgentCapabilities.AgentCapabilities_ReportsRemoteConfig_VALUE);
+  }
 }
