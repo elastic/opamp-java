222186784db58598fa11989e0fcc3fefe511e1b3
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-07-10 14:37:14 +0200

    Implementing AgentDescriptionVisitor

--- src/main/java/io/opentelemetry/opamp/client/visitors/AgentDescriptionVisitor.java
+++ src/main/java/io/opentelemetry/opamp/client/visitors/AgentDescriptionVisitor.java
@@ -1,8 +1,31 @@
 package io.opentelemetry.opamp.client.visitors;
 
+import java.util.ArrayList;
+import java.util.List;
+import opamp.proto.Anyvalue;
 import opamp.proto.Opamp;
 
 public class AgentDescriptionVisitor implements AgentToServerVisitor {
+  private final List<Anyvalue.KeyValue> identifyingAttributes = new ArrayList<>();
+
+  public AgentDescriptionVisitor(String serviceName, String serviceVersion) {
+    identifyingAttributes.add(createKeyValue("service.name", serviceName));
+    identifyingAttributes.add(createKeyValue("service.version", serviceVersion));
+  }
+
+  private Anyvalue.KeyValue createKeyValue(String key, String value) {
+    return Anyvalue.KeyValue.newBuilder()
+        .setKey(key)
+        .setValue(Anyvalue.AnyValue.newBuilder().setStringValue(value).build())
+        .build();
+  }
+
   @Override
-  public void visit(Opamp.AgentToServer.Builder builder) {}
+  public void visit(Opamp.AgentToServer.Builder builder) {
+    Opamp.AgentDescription agentDescription =
+        Opamp.AgentDescription.newBuilder()
+            .addAllIdentifyingAttributes(identifyingAttributes)
+            .build();
+    builder.setAgentDescription(agentDescription);
+  }
 }
