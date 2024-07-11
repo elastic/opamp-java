4cd83c954526de4170d32f354c714236898dbf8b
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-07-11 10:29:05 +0200

    Implementing FlagsVisitor

--- src/main/java/io/opentelemetry/opamp/client/visitors/FlagsVisitor.java
+++ src/main/java/io/opentelemetry/opamp/client/visitors/FlagsVisitor.java
@@ -3,6 +3,9 @@ package io.opentelemetry.opamp.client.visitors;
 import opamp.proto.Opamp;
 
 public class FlagsVisitor implements AgentToServerVisitor {
+
   @Override
-  public void visit(Opamp.AgentToServer.Builder builder) {}
+  public void visit(Opamp.AgentToServer.Builder builder) {
+    builder.setFlags(Opamp.AgentToServerFlags.AgentToServerFlags_Unspecified_VALUE);
+  }
 }
