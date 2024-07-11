721fda744105ddc14b41f65bf49412dd7a09e37f
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-07-11 19:16:20 +0200

    Using ContextBuilder

--- src/main/java/co/elastic/opamp/client/internal/OpampClientImpl.java
+++ src/main/java/co/elastic/opamp/client/internal/OpampClientImpl.java
@@ -1,18 +1,25 @@
 package co.elastic.opamp.client.internal;
 
 import co.elastic.opamp.client.OpampClient;
-import co.elastic.opamp.client.requests.OpampService;
 import co.elastic.opamp.client.internal.visitors.AgentToServerVisitor;
+import co.elastic.opamp.client.requests.OpampService;
 import java.io.IOException;
 import java.util.List;
+import java.util.function.Supplier;
 import opamp.proto.Opamp;
 
 public class OpampClientImpl implements OpampClient {
   private final OpampService service;
+  private final Supplier<ClientContext.Builder> contextBuilderSupplier;
   private final List<AgentToServerVisitor> visitors;
+  private ClientContext.Builder contextBuilder;
 
-  OpampClientImpl(OpampService service, List<AgentToServerVisitor> visitors) {
+  OpampClientImpl(
+      OpampService service,
+      Supplier<ClientContext.Builder> contextBuilderSupplier,
+      List<AgentToServerVisitor> visitors) {
     this.service = service;
+    this.contextBuilderSupplier = contextBuilderSupplier;
     this.visitors = visitors;
   }
 
@@ -23,12 +30,13 @@ public class OpampClientImpl implements OpampClient {
 
   @Override
   public void stop() {
+    getContextBuilder().stop();
     sendMessage();
   }
 
   private Opamp.AgentToServer buildMessage() {
     Opamp.AgentToServer.Builder builder = Opamp.AgentToServer.newBuilder();
-    visitors.forEach(visitor -> visitor.visit(builder));
+    visitors.forEach(visitor -> visitor.visit(buildContext(), builder));
     return builder.build();
   }
 
@@ -38,5 +46,19 @@ public class OpampClientImpl implements OpampClient {
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  private ClientContext buildContext() {
+    ClientContext context = getContextBuilder().build();
+    contextBuilder = null;
+    return context;
+  }
+
+  private ClientContext.Builder getContextBuilder() {
+    if (contextBuilder == null) {
+      contextBuilder = contextBuilderSupplier.get();
+    }
+
+    return contextBuilder;
   }
 }
