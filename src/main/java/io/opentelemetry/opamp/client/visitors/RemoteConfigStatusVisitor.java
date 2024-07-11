ebb81e65ca3ef1112c55cfd2f370a5f2268611d9
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-07-11 10:23:33 +0200

    Implementing RemoteConfigStatusVisitor

--- src/main/java/io/opentelemetry/opamp/client/visitors/RemoteConfigStatusVisitor.java
+++ src/main/java/io/opentelemetry/opamp/client/visitors/RemoteConfigStatusVisitor.java
@@ -3,6 +3,14 @@ package io.opentelemetry.opamp.client.visitors;
 import opamp.proto.Opamp;
 
 public class RemoteConfigStatusVisitor implements AgentToServerVisitor {
+
   @Override
-  public void visit(Opamp.AgentToServer.Builder builder) {}
+  public void visit(Opamp.AgentToServer.Builder builder) {
+    Opamp.RemoteConfigStatus status =
+        Opamp.RemoteConfigStatus.newBuilder()
+            .setStatus(Opamp.RemoteConfigStatuses.RemoteConfigStatuses_UNSET)
+            .build();
+
+    builder.setRemoteConfigStatus(status);
+  }
 }
