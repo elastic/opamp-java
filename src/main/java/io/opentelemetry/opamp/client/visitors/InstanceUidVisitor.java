f3be8bca1cea05e9f472001644717ec8b3b7d04f
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-07-11 15:32:28 +0200

    Properly implementing InstanceUidVisitor

--- src/main/java/io/opentelemetry/opamp/client/visitors/InstanceUidVisitor.java
+++ src/main/java/io/opentelemetry/opamp/client/visitors/InstanceUidVisitor.java
@@ -1,14 +1,27 @@
 package io.opentelemetry.opamp.client.visitors;
 
 import com.google.protobuf.ByteString;
+import java.nio.ByteBuffer;
 import java.util.UUID;
 import opamp.proto.Opamp;
 
 public class InstanceUidVisitor implements AgentToServerVisitor {
-  private final String uuid = UUID.randomUUID().toString();
+  private byte[] uuid;
 
   @Override
   public void visit(Opamp.AgentToServer.Builder builder) {
-    builder.setInstanceUid(ByteString.copyFromUtf8(uuid));
+    builder.setInstanceUid(ByteString.copyFrom(getUuid()));
+  }
+
+  private byte[] getUuid() {
+    if (uuid == null) {
+      UUID random = UUID.randomUUID();
+      ByteBuffer buffer = ByteBuffer.allocate(16);
+      buffer.putLong(random.getMostSignificantBits());
+      buffer.putLong(random.getLeastSignificantBits());
+      uuid = buffer.array();
+    }
+
+    return uuid;
   }
 }
