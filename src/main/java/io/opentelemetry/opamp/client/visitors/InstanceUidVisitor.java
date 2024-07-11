e33391140ce2c6685d9e48257e830987e541035f
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-07-11 15:15:01 +0200

    Using single uuid

--- src/main/java/io/opentelemetry/opamp/client/visitors/InstanceUidVisitor.java
+++ src/main/java/io/opentelemetry/opamp/client/visitors/InstanceUidVisitor.java
@@ -5,9 +5,10 @@ import java.util.UUID;
 import opamp.proto.Opamp;
 
 public class InstanceUidVisitor implements AgentToServerVisitor {
+  private final String uuid = UUID.randomUUID().toString();
 
   @Override
   public void visit(Opamp.AgentToServer.Builder builder) {
-    builder.setInstanceUid(ByteString.copyFromUtf8(UUID.randomUUID().toString()));
+    builder.setInstanceUid(ByteString.copyFromUtf8(uuid));
   }
 }
