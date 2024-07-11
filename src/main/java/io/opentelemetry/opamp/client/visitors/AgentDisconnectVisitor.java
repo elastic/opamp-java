ccbdf63cab0d31d4fa6818120256b5423368a977
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-07-11 10:26:17 +0200

    Implementing AgentDisconnectVisitor

--- src/main/java/io/opentelemetry/opamp/client/visitors/AgentDisconnectVisitor.java
+++ src/main/java/io/opentelemetry/opamp/client/visitors/AgentDisconnectVisitor.java
@@ -3,6 +3,9 @@ package io.opentelemetry.opamp.client.visitors;
 import opamp.proto.Opamp;
 
 public class AgentDisconnectVisitor implements AgentToServerVisitor {
+
   @Override
-  public void visit(Opamp.AgentToServer.Builder builder) {}
+  public void visit(Opamp.AgentToServer.Builder builder) {
+    builder.setAgentDisconnect(Opamp.AgentDisconnect.newBuilder().build());
+  }
 }
