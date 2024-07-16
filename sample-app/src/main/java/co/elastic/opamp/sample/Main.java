373ba64e8bd100a76c36e85aea9a1021ed2da951
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-07-16 10:59:17 +0200

    Created sample app

---
+++ sample-app/src/main/java/co/elastic/opamp/sample/Main.java
@@ -0,0 +1,31 @@
+package co.elastic.opamp.sample;
+
+import co.elastic.opamp.client.OpampClient;
+import co.elastic.opamp.client.response.Response;
+import opamp.proto.Opamp;
+
+public class Main {
+  public static void main(String[] args) {
+    OpampClient client =
+        OpampClient.builder()
+            .build(
+                new OpampClient.Callback() {
+                  @Override
+                  public void onConnect(OpampClient client) {
+                    client.stop();
+                  }
+
+                  @Override
+                  public void onConnectFailed(OpampClient client, Throwable throwable) {}
+
+                  @Override
+                  public void onErrorResponse(
+                      OpampClient client, Opamp.ServerErrorResponse errorResponse) {}
+
+                  @Override
+                  public void onMessage(OpampClient client, Response response) {}
+                });
+
+    client.start();
+  }
+}
