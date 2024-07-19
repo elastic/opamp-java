c041caa24fa371826e004a4791c36c9a7beab90b
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-07-19 14:59:04 +0200

    Adding docs

--- client/src/main/java/co/elastic/opamp/client/internal/request/handlers/sleeper/Sleeper.java
+++ client/src/main/java/co/elastic/opamp/client/internal/request/handlers/sleeper/Sleeper.java
@@ -2,6 +2,7 @@ package co.elastic.opamp.client.internal.request.handlers.sleeper;
 
 import co.elastic.opamp.client.internal.request.handlers.sleeper.impl.SleeperImpl;
 
+/** Wrapper for {@link Object#wait()} and {@link Object#notify()} to make them testable. */
 public interface Sleeper {
   static Sleeper create() {
     return new SleeperImpl();
