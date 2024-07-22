733aa5584d8c5745d32f967d0c9750bf99dc4526
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-07-22 18:12:01 +0200

    Adding sample app code

--- sample-app/src/main/java/co/elastic/opamp/sample/Main.java
+++ sample-app/src/main/java/co/elastic/opamp/sample/Main.java
@@ -2,30 +2,39 @@ package co.elastic.opamp.sample;
 
 import co.elastic.opamp.client.OpampClient;
 import co.elastic.opamp.client.response.MessageData;
+import java.util.concurrent.CountDownLatch;
 import opamp.proto.Opamp;
 
 public class Main {
-  public static void main(String[] args) {
-    OpampClient client =
-        OpampClient.builder()
-            .build(
-                new OpampClient.Callback() {
-                  @Override
-                  public void onConnect(OpampClient client) {
-                    client.stop();
-                  }
+  public static void main(String[] args) throws InterruptedException {
+    CountDownLatch latch = new CountDownLatch(1);
+    new Thread(
+            () -> {
+              OpampClient client =
+                  OpampClient.builder()
+                      .build(
+                          new OpampClient.Callback() {
+                            @Override
+                            public void onConnect(OpampClient client) {
+                              client.stop();
+                              latch.countDown();
+                            }
 
-                  @Override
-                  public void onConnectFailed(OpampClient client, Throwable throwable) {}
+                            @Override
+                            public void onConnectFailed(OpampClient client, Throwable throwable) {}
 
-                  @Override
-                  public void onErrorResponse(
-                      OpampClient client, Opamp.ServerErrorResponse errorResponse) {}
+                            @Override
+                            public void onErrorResponse(
+                                OpampClient client, Opamp.ServerErrorResponse errorResponse) {}
 
-                  @Override
-                  public void onMessage(OpampClient client, MessageData messageData) {}
-                });
+                            @Override
+                            public void onMessage(OpampClient client, MessageData messageData) {}
+                          });
 
-    client.start();
+              client.start();
+            })
+        .start();
+
+    latch.await();
   }
 }
