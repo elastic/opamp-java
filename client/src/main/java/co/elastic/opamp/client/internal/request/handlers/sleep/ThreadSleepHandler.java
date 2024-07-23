e03640b5d6fdf88350992f5276e5e46828753e66
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-07-23 08:24:15 +0200

    Adding javadoc

--- client/src/main/java/co/elastic/opamp/client/internal/request/handlers/sleep/ThreadSleepHandler.java
+++ client/src/main/java/co/elastic/opamp/client/internal/request/handlers/sleep/ThreadSleepHandler.java
@@ -1,7 +1,17 @@
 package co.elastic.opamp.client.internal.request.handlers.sleep;
 
+/** Utility to lock the polling thread between loops for a period of time. */
 public interface ThreadSleepHandler {
+  /**
+   * If the thread is locked, release it right away. If the thread isn't locked, then ignore the
+   * next call to {@link #sleep()}.
+   */
   void awakeOrIgnoreNextSleep();
 
+  /**
+   * Locks the thread for a period of time or until {@link #awakeOrIgnoreNextSleep()} is called.
+   *
+   * @throws InterruptedException When the thread is interrupted while locked.
+   */
   void sleep() throws InterruptedException;
 }
