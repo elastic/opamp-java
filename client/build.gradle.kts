1c885ea59f4a96bdcb9579209ce105351c7be1ea
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-07-30 09:20:20 +0200

    Adding spotless license

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -15,6 +15,7 @@ sourceSets {
 spotless {
     java {
         target("src/*/java/**/*.java")
+        licenseHeader(getLicenseHeader())
     }
 }
 
@@ -22,6 +23,27 @@ afterEvaluate {
     tasks.named("classes").configure {
         dependsOn("spotlessApply")
     }
+}
+
+fun getLicenseHeader(): String {
+    return "/*\n" +
+            " * Licensed to Elasticsearch B.V. under one or more contributor\n" +
+            " * license agreements. See the NOTICE file distributed with\n" +
+            " * this work for additional information regarding copyright\n" +
+            " * ownership. Elasticsearch B.V. licenses this file to you under\n" +
+            " * the Apache License, Version 2.0 (the \"License\"); you may\n" +
+            " * not use this file except in compliance with the License.\n" +
+            " * You may obtain a copy of the License at\n" +
+            " *\n" +
+            " *\thttp://www.apache.org/licenses/LICENSE-2.0\n" +
+            " *\n" +
+            " * Unless required by applicable law or agreed to in writing,\n" +
+            " * software distributed under the License is distributed on an\n" +
+            " * \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY\n" +
+            " * KIND, either express or implied.  See the License for the\n" +
+            " * specific language governing permissions and limitations\n" +
+            " * under the License.\n" +
+            " */\n"
 }
 
 dependencies {
