dc2cb6677bd550eedb56a5be42d508a9b2647bee
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-08-01 16:35:01 +0200

    Validating central config processing use cases

--- elastic-client/src/main/java/co/elastic/opamp/client/CentralConfigurationManager.java
+++ elastic-client/src/main/java/co/elastic/opamp/client/CentralConfigurationManager.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package co.elastic.opamp.client;
 
 import co.elastic.opamp.client.response.MessageData;
@@ -43,13 +61,18 @@ public final class CentralConfigurationManager implements OpampClient.Callback {
     Map<String, Opamp.AgentConfigFile> configMapMap = remoteConfig.getConfig().getConfigMapMap();
     Opamp.AgentConfigFile centralConfig = configMapMap.get("");
     if (centralConfig != null) {
-      CentralConfigurationProcessor.Result result =
-          processor.process(parseCentralConfiguration(centralConfig.getBody()));
+      Map<String, String> configuration = parseCentralConfiguration(centralConfig.getBody());
+      Opamp.RemoteConfigStatuses status;
 
-      Opamp.RemoteConfigStatuses status =
-          (result == CentralConfigurationProcessor.Result.APPLIED)
-              ? Opamp.RemoteConfigStatuses.RemoteConfigStatuses_APPLIED
-              : Opamp.RemoteConfigStatuses.RemoteConfigStatuses_FAILED;
+      if (configuration != null) {
+        CentralConfigurationProcessor.Result result = processor.process(configuration);
+        status =
+            (result == CentralConfigurationProcessor.Result.SUCCESS)
+                ? Opamp.RemoteConfigStatuses.RemoteConfigStatuses_APPLIED
+                : Opamp.RemoteConfigStatuses.RemoteConfigStatuses_FAILED;
+      } else {
+        status = Opamp.RemoteConfigStatuses.RemoteConfigStatuses_FAILED;
+      }
 
       client.setRemoteConfigStatus(getRemoteConfigStatus(status));
     }
@@ -66,7 +89,7 @@ public final class CentralConfigurationManager implements OpampClient.Callback {
       return Collections.unmodifiableMap(MapConverter.deserialize(reader));
     } catch (IOException e) {
       logger.log(Level.WARNING, "Could not parse central configuration.", e);
-      return Collections.emptyMap();
+      return null;
     }
   }
 
