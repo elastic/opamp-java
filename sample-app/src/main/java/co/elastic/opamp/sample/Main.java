18516ba44eda0d9658fa57d0525c19df1daecc7a
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-08-07 10:49:36 +0200

    Adding logs and making sample service name configurable from cmd

--- sample-app/src/main/java/co/elastic/opamp/sample/Main.java
+++ sample-app/src/main/java/co/elastic/opamp/sample/Main.java
@@ -2,16 +2,23 @@ package co.elastic.opamp.sample;
 
 import co.elastic.opamp.client.CentralConfigurationManager;
 import co.elastic.opamp.client.CentralConfigurationProcessor;
+import java.time.Duration;
 import java.util.logging.Logger;
 
 public class Main {
   private static final Logger logger = Logger.getLogger(Main.class.getName());
 
   public static void main(String[] args) {
+    String serviceName = "some-service";
+    if (args.length > 0) {
+      serviceName = args[0];
+    }
+    logger.info("============= Starting client for: " + serviceName);
     CentralConfigurationManager centralConfigurationManager =
         CentralConfigurationManager.builder()
-            .setServiceName("my-service")
+            .setServiceName(serviceName)
             .setServiceVersion("1.0.0")
+            .setPollingInterval(Duration.ofSeconds(5))
             .build();
 
     centralConfigurationManager.start(
@@ -20,6 +27,12 @@ public class Main {
           return CentralConfigurationProcessor.Result.SUCCESS;
         });
 
-    Runtime.getRuntime().addShutdownHook(new Thread(centralConfigurationManager::stop));
+    Runtime.getRuntime()
+        .addShutdownHook(
+            new Thread(
+                () -> {
+                  logger.info("=========== Shutting down");
+                  centralConfigurationManager.stop();
+                }));
   }
 }
