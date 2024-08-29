53c3ae0ea0559c034b5e42cb7540c7ee5226bad1
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-08-29 14:51:20 +0200

    Adding sample logs

--- sample-app/src/main/java/co/elastic/opamp/sample/MainWebsocket.java
+++ sample-app/src/main/java/co/elastic/opamp/sample/MainWebsocket.java
@@ -9,22 +9,29 @@ public class MainWebsocket {
   private static final Logger logger = Logger.getLogger(MainWebsocket.class.getName());
 
   public static void main(String[] args) {
-    OpampClient client = OpampClient.webSocketBuilder().enableRemoteConfig().build();
+    OpampClient client = OpampClient.httpBuilder().enableRemoteConfig().build();
 
     client.start(
         new OpampClient.Callback() {
           @Override
-          public void onConnect(OpampClient client) {}
+          public void onConnect(OpampClient client) {
+            logger.info("Client connected");
+          }
 
           @Override
-          public void onConnectFailed(OpampClient client, Throwable throwable) {}
+          public void onConnectFailed(OpampClient client, Throwable throwable) {
+            logger.info("Client connect failed: " + throwable);
+          }
 
           @Override
-          public void onErrorResponse(
-              OpampClient client, Opamp.ServerErrorResponse errorResponse) {}
+          public void onErrorResponse(OpampClient client, Opamp.ServerErrorResponse errorResponse) {
+            logger.info("Server error: " + errorResponse);
+          }
 
           @Override
-          public void onMessage(OpampClient client, MessageData messageData) {}
+          public void onMessage(OpampClient client, MessageData messageData) {
+            logger.info("Message received: " + messageData);
+          }
         });
   }
 }
